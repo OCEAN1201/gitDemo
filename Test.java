b5ffd9674e86e77ae7706becc0420f1add0f0495
zy <zy@qq.com>
2022-11-01 19:02:16 +0800

    first commit

---
+++ Test.java
@@ -0,0 +1,13 @@
+/**
+ * Project：gitDemo
+ * Package：PACKAGE_NAME
+ * Author:zy
+ * Time:2022/11/1
+ */
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是git");
+    }
+}
